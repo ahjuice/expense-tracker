e986a602843acf535483c8336e9d581f951e9ebc
Chris Agius <cagius@2u.com>
2018-11-06 11:32:29 -0500

    fix spec helper, flesh out API spec, add API initializer

--- app/api.rb
+++ app/api.rb
@@ -4,6 +4,11 @@ require 'json'
 module ExpenseTracker
   # JSON API for interfacing with expense tracker
   class API < Sinatra::Base
+    def initialize(ledger: Ledger.new)
+      @ledger = ledger
+      super()
+    end
+
     post '/expenses' do
       JSON.generate('expense_id' => 42)
     end
